2f83f7caa659537af4e3da26b064ddb8ed42f628
Juan Ribot <juanribot2@gmail.com>
2023-07-28 10:52:31 -0500

    Added ; to sound script

--- Assets/Sonidos/PlaySoundOnStart.cs
+++ Assets/Sonidos/PlaySoundOnStart.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlaySoundOnStart : MonoBehaviour
 {
-    [SerializeField] private AudioClip _clip
+    [SerializeField] private AudioClip _clip;
 	// Start is called before the first frame update
     void Start()
     {
